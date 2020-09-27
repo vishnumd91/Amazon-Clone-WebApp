6e8e363e330aa7ff9ee11912002bf57a13256cb0
vishnumd91 <vishnudivakaran1@gmail.com>
2020-09-27 22:16:56 +0530

    Created Nav Bar Component with necessary routing links

---
+++ src/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <nav className = 'header'>
+            {/* Amazon Logo with link to home page */}
+            <Link to = '/'>
+                <img className = 'header__logo'
+                src = 'http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                alt = '' />
+            </Link>
+
+            {/* Search Box with search Icon */}
+            <div className = 'header__search'>
+                <input type = 'text' className = 'header__searchInput'></input>
+                <SearchIcon className = 'header__searchIcon'></SearchIcon>
+            </div>
+
+            {/* 4 Links in nav bar with cart symbol */}
+            <div className = 'header__nav'>
+            <Link to = '/login' className = 'header__link'>
+                <div className = 'header__option'>
+                    <span className = 'optionLineOne'>Hello</span>
+                    <span className = 'optionLineTwo'>Sign In</span>
+                </div>
+               
+            </Link>
+
+            <Link to = '/' className = 'header__link'>
+                <div className = 'header__option'>
+                <span className = 'optionLineOne'>Returns</span>
+                <span className = 'optionLineTwo'>& Orders</span>
+                </div>
+            </Link>
+
+            <Link to = '/checkout' className = 'header__link'>
+                <div className = 'header__option'>
+                <span className = 'optionLineOne'>Your</span>
+                <span className = 'optionLineTwo'>Prime</span>
+                </div>
+            </Link>
+
+            <Link to = '/checkout'>
+                <div className = 'header__optionBasket'>
+                    <ShoppingBasketIcon></ShoppingBasketIcon>
+                    <span className = 'header__optionLineTwo header__basketCount'>0</span>
+                </div>
+            </Link>
+
+            </div>
+           
+        </nav>
+    )
+}
+
+export default Header
