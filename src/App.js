e305871843b2ed110cee875ab8c6be32c9e4162f
vishnumd91 <vishnudivakaran1@gmail.com>
2020-09-27 12:39:32 +0530

    Setting up of React project for amazon clone and Routing is completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <div className="App">
+         <Switch>
+           <Route path = '/checkout'>
+             <h1>CHECKOUT</h1>
+           </Route>
+
+           <Route path = '/login'>
+             <h1>LOGIN PAGE</h1>
+           </Route>
+
+           <Route path = '/'>
+             <h1>HOMEPAGE</h1>
+           </Route>
+           </Switch>
+       </div>
+    </Router>
   );
 }
 
