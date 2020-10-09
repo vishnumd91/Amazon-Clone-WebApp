0f4c7ff55750d29fa2f999112f5bace9f2473519
Vishnu MD <vishnudivakaran1@gmail.com>
2020-10-09 21:19:57 +0530

    Update Home.js
    Changed the price of the products which seems to be sensible.

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ function Home() {
             {/* First Row Products */}
             <div className = 'home__row'>
                 <Product 
-                    id = '12345'
+                    id = '456231534'
                     title = 'The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
-                    price = {11}
+                    price = {1299}
                     rating = {5}
                     image = 'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
                 />
@@ -19,7 +19,7 @@ function Home() {
                 <Product
                     id="49538094"
                     title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-                    price={239.0}
+                    price={2598.0}
                     rating={4}
                     image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
                 />
@@ -30,21 +30,21 @@ function Home() {
                 <Product
                     id="4903850"
                     title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-                    price={199.99}
+                    price={21299.99}
                     rating={3}
                     image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
                 />
                 <Product
                     id="23445930"
                     title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-                    price={98.99}
+                    price={29898.99}
                     rating={5}
                     image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
                 />
                 <Product
                     id="3254354345"
                     title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-                    price={598.99}
+                    price={32798.99}
                     rating={4}
                     image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
                 />
@@ -55,7 +55,7 @@ function Home() {
                 <Product
                     id="90829332"
                     title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-                    price={1094.98}
+                    price={41094.98}
                     rating={4}
                     image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
                 />
