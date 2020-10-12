b31e8b9139911060fdd0193422c858aafe8f6115
vishnumd91 <vishnudivakaran1@gmail.com>
2020-10-12 16:35:46 +0530

    Added Login Component with firebase issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from 'firebase';
 
 function App() {
+  const [ {}, dispatch] = useStateValue();
+
+  // useEffect Hook is used when we need to run some operation based on a given condition
+  // const unsubscribe = useEffect(() => {
+
+  //   auth.onAuthStateChanged((authUser) => {
+
+  //     if (authUser) {
+  //       // User is logged in
+  //       dispatch( {
+  //         type: 'SET_USER',
+  //         user: authUser,
+  //       })
+  //     }
+  //     else {
+  //       // User is logged out
+  //       dispatch( {
+  //         type: 'SET_USER',
+  //         user: null,
+  //       })
+  //     }
+  //   })
+
+  //   return () => {
+  //     // cleanup
+  //     unsubscribe();
+  //   }
+  // }, [])
+  // console.log('User =>', user);
+
   return (
     <Router>
        <div className="App">
@@ -16,7 +49,7 @@ function App() {
            </Route>
 
            <Route path = '/login'>
-             <h1>LOGIN PAGE</h1>
+             <Login />
            </Route>
 
            <Route path = '/'>
